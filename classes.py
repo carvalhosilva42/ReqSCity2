d5622e36c4c9d2d3076020b49a1d6e5ae1fd07fb
Bruno <carvalhosilva42@hotmail.com>
2023-03-27 16:47:57 -0300

    Acrescentando Algoritmos de ambiguidade léxica

--- classes.py
+++ classes.py
@@ -87,3 +87,109 @@ class AnaliseSintatica:
         print("Cheguei")
         self.analise_completa()
         return self.analise_sintatica
+
+def limpeza(requisitos):
+    tokens = []
+    for indice,req in enumerate(requisitos):
+        req= re.sub("[!'@*>=+#%:&;™.,_\\/()?\"]+", ' ', req)
+        req= re.sub('[0-9]+', ' ', req)
+        req= re.sub('- | -|-+', ' ', req)
+        req= re.sub(r'(?:^| \\ )\w(?:$| )', ' ', req).strip()
+        words = nltk.word_tokenize(req.lower())
+        words = words[1:]
+        tokens.append(words)
+        newwords = []
+        for word in words:
+            if word in nltk.corpus.stopwords.words('english'):
+                continue
+            try:
+                newwords.append(word)
+            except:
+                continue
+        requisitos[indice] = ' '.join(newwords)
+    return tokens,requisitos
+
+
+class ambiguidade_lexica:
+    def __init__(self, requisitos, palavras_amb,POS):
+        self.requisitos=requisitos
+        self.ambiguos_lexicos = {"PA": [], "AFL": []}
+        self.palavras_amb=palavras_amb
+        self.POS=POS
+        self.tokens=[]
+    
+    def palavras_ambiguas(self):
+        for indice,req in enumerate(self.requisitos):
+            self.tokens.append(nltk.word_tokenize(req))
+            for ambigua in self.palavras_amb:
+                if ambigua.lower() in tokens:
+                    self.ambiguos_lexicos["PA"].append(indice)
+
+    def objetoWN(self,pos):
+        
+        if pos == 'V':
+            return nltk.corpus.wordnet.VERB
+        elif pos == 'N':
+            return nltk.corpus.wordnet.NOUN
+        elif pos == 'R':
+            return nltk.corpus.wordnet.ADV
+        elif pos == 'J':
+            return nltk.corpus.wordnet.ADJ
+        else:
+            return nltk.corpus.wordnet.NOUN
+    
+    def algoritmo_flex_amb(self):
+        LIMIAR_MIN = 3 / 4
+        QTDE_SIM = 3
+        for indice,req in enumerate(self.POS):
+            qtde = len(self.tokens[indice])
+            possiveis = []
+            for token in req:
+                if len(nltk.corpus.wordnet.synsets(token[0], pos=self.objetoWN(token[1][0]))) > QTDE_SIM:
+                    possiveis.append(req[0])
+            if len(possiveis) >= (LIMIAR_MIN * qtde):
+                self.ambiguos_lexicos["AFL"].append(indice)
+    
+    def requisitos_ambiguos(self):
+        self.palavras_ambiguas()
+        self.algoritmo_flex_amb()
+        return self.ambiguos_lexicos
+
+class Ambiguidade_sintatica
+    
+
+
+arquivo = open('requisitos.txt','r')
+texto = ''
+for linhas in arquivo:
+    texto+=linhas
+arquivo.close()
+requisitos = texto.split('\n')
+
+arquivo = open('dicionario_base.txt','r')
+texto = ''
+for linhas in arquivo:
+    texto+=linhas
+arquivo.close()
+palavras_ambiguas = texto.split('\n')
+
+
+
+def trigram_pos(requisitos):
+    from pickle import load
+    entrada = open('trigram.pkl','rb')
+    tagger = load(entrada)
+    entrada.close()
+    
+    saida=[tagger.tag(requisito) for requisito in requisitos]
+    
+    return saida
+
+
+tokens,requisitos=limpeza(requisitos)
+pos = trigram_pos(tokens)
+
+ambiguidadelexica = ambiguidade_lexica(requisitos,palavras_ambiguas,pos).requisitos_ambiguos()
+print(ambiguidadelexica)
+            
+        
